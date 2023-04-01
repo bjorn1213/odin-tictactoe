606509ae5dfd039c2bac84a9acc0a07ac298b9eb
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-04-01 16:23:15 +0200

    Set up basic UI and interaction

--- script.js
+++ script.js
@@ -41,32 +41,74 @@ const gameBoard = (() => {
     return true;
   };
 
-  return { getBoardRepresentation, playOnSpot };
+  const getPositionValue = (i, j) => _board[i][j];
+
+  return { getBoardRepresentation, playOnSpot, getPositionValue };
 })();
 
 const displayController = (() => {
   let _currentPlayerIndex = 0;
+  let _players = [];
+  let _board;
 
   const setPlayers = (player1, player2) => {
-    this.players = [player1, player2];
-  };
-
-  const setBoard = (board) => {
-    this.board = board;
+    _players = [player1, player2];
   };
 
   const printPlayers = () => {
-    for (const player of this.players) {
+    for (const player of _players) {
       console.log(`Name: ${player.name}`);
     }
   };
 
   const nextPlayerPlaysOnBoard = (i, j) => {
-    if (this.board.playOnSpot(i, j, _currentPlayerIndex)) {
+    if (_board.playOnSpot(i, j, _currentPlayerIndex)) {
       _currentPlayerIndex = 1 - _currentPlayerIndex; // switch active player
     }
+  };
 
-    console.log(this.board.getBoardRepresentation());
+  const displayHtmlBoard = () => {
+    const pageContainer = document.getElementById("pagecontainer");
+
+    const gameContainer = document.createElement("div");
+    gameContainer.setAttribute("id", "gamecontainer");
+    gameContainer.setAttribute("class", "gamecontainer");
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const gameSquare = document.createElement("div");
+        gameSquare.setAttribute("row", i);
+        gameSquare.setAttribute("col", j);
+        gameSquare.setAttribute("class", "gamesquare empty");
+        if (_board.getPositionValue(i, j) === undefined) {
+          gameSquare.setAttribute("class", "gamesquare empty");
+        } else if (_board.getPositionValue(i, j)) {
+          gameSquare.setAttribute("class", "gamesquare");
+          gameSquare.innerText = "X";
+        } else {
+          gameSquare.setAttribute("class", "gamesquare");
+          gameSquare.innerText = "O";
+        }
+
+        gameSquare.addEventListener("click", (event) => {
+          const tgt = event.target;
+          nextPlayerPlaysOnBoard(
+            tgt.getAttribute("row"),
+            tgt.getAttribute("col")
+          );
+          displayHtmlBoard();
+        });
+
+        gameContainer.appendChild(gameSquare);
+      }
+    }
+
+    pageContainer.replaceChildren(gameContainer);
+  };
+
+  const setBoard = (board) => {
+    _board = board;
+    displayHtmlBoard();
   };
 
   return { setPlayers, setBoard, printPlayers, nextPlayerPlaysOnBoard };
@@ -83,5 +125,3 @@ const player2 = Player("Jonasson", 1);
 
 displayController.setPlayers(player1, player2);
 displayController.setBoard(gameBoard);
-
-displayController.nextPlayerPlaysOnBoard(0, 0);
